ce96a061e33eb02ee60d89e12550fbec0423d73f
brightboost <info@brightboost.nl>
2021-07-31 22:22:22 +0200

    setting up junit for eclipse

---
+++ setup-junit-eclipse/src/main/java/com/example/setup_junit_eclipse/App.java
@@ -0,0 +1,13 @@
+package com.example.setup_junit_eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ setup-junit-eclipse/src/test/java/com/example/setup_junit_eclipse/AppTest.java
@@ -0,0 +1,20 @@
+package com.example.setup_junit_eclipse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

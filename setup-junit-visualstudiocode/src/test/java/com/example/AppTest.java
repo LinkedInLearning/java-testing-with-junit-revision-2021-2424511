2bf8ad7a21dcad75d33455586a9367a614779c6b
brightboost <info@brightboost.nl>
2021-07-31 20:34:23 +0200

    setting up junit for vscode

---
+++ setup-junit-visualstudiocode/src/test/java/com/example/AppTest.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

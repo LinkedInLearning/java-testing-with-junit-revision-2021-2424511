9ed1273ffffa0f82f9b2c72eb1032b6cc93191f5
brightboost <info@brightboost.nl>
2021-08-01 22:52:55 +0200

    adding custom names using displayname

--- example-junit/src/test/java/BankAccountTest.java
+++ example-junit/src/test/java/BankAccountTest.java
@@ -1,10 +1,13 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Test BankAccount class")
 public class BankAccountTest {
 
     @Test
+    @DisplayName("Withdraw 500 successfully")
     public void testWithdraw() {
         BankAccount bankAccount = new BankAccount(500, -1000);
         bankAccount.withdraw(300);
@@ -12,6 +15,7 @@ public class BankAccountTest {
     }
 
     @Test
+    @DisplayName("Deposit 400 successfully")
     public void testDeposit() {
         BankAccount bankAccount = new BankAccount(400, 0);
         bankAccount.deposit(500);

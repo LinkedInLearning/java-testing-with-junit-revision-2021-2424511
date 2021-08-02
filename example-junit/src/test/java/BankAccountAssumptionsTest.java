ebf6a1ba8c47331a42d12eca43827846ee26dff1
brightboost <info@brightboost.nl>
2021-08-02 13:47:17 +0200

    assumptions demo

--- example-junit/src/test/java/BankAccountAssumptionsTest.java
+++ example-junit/src/test/java/BankAccountAssumptionsTest.java
@@ -2,12 +2,28 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.*;
 
 public class BankAccountAssumptionsTest {
     @Test
     @DisplayName("Test activation account after creation")
-    public void testActive() {
+    public void testActive0() {
         BankAccount bankAccount = new BankAccount(500, 0);
+        assumeTrue(bankAccount != null);
+        assertTrue(bankAccount.isActive());    }
+
+    @Test
+    @DisplayName("Test activation account after creation")
+    public void testActive1() {
+        BankAccount bankAccount = new BankAccount(500, 0);
+        assumeFalse(bankAccount == null);
         assertTrue(bankAccount.isActive());
+    }
+
+    @Test
+    @DisplayName("Test activation account after creation")
+    public void testActive2() {
+        BankAccount bankAccount = new BankAccount(500, 0);
+        assumingThat(bankAccount == null, () -> assertTrue(bankAccount.isActive()));
     }
 }

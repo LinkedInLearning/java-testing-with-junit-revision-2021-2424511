e0ef2403119b98234e90152a7bae8cf55410e6ef
brightboost <info@brightboost.nl>
2021-08-03 00:45:06 +0200

    beginning point demo disabled

---
+++ example-junit/src/test/java/BankAccountDisabledTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(BankAccountParameterResolver.class)
+public class BankAccountDisabledTest {
+
+    @Test
+    @DisplayName("Deposit 500 successfully")
+    public void testDeposit(BankAccount bankAccount) {
+        bankAccount.deposit(500);
+        assertEquals(500, bankAccount.getBalance());
+    }
+
+}

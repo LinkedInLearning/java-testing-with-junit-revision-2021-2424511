511276e75ea7ca93ba7e101ac475bfb7a50c836b
brightboost <info@brightboost.nl>
2021-08-02 12:08:10 +0200

    beginning of the assertions explanation with starting point class in place

---
+++ example-junit/src/test/java/BankAccountAssertionsTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BankAccountAssertionsTest {
+    @Test
+    @DisplayName("Withdraw 500 successfully")
+    public void testWithdraw() {
+        BankAccount bankAccount = new BankAccount(500, -1000);
+        bankAccount.withdraw(300);
+        assertEquals(200, bankAccount.getBalance());
+    }
+
+    @Test
+    @DisplayName("Deposit 400 successfully")
+    public void testDeposit() {
+        BankAccount bankAccount = new BankAccount(400, 0);
+        bankAccount.deposit(500);
+        assertEquals(900, bankAccount.getBalance());
+    }
+}

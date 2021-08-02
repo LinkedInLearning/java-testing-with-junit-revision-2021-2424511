5ec8e4df93acb584a6435e70021525685f3f743f
brightboost <info@brightboost.nl>
2021-08-03 00:42:28 +0200

    conditional tests demo

--- example-junit/src/test/java/BankAccountConditionalTest.java
+++ example-junit/src/test/java/BankAccountConditionalTest.java
@@ -1,17 +1,28 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
+
 public class BankAccountConditionalTest {
 
+    @Test
+    @EnabledOnOs({OS.MAC})
     public void testMac() {
 
     }
 
+    @Test
+    @EnabledOnOs({OS.WINDOWS})
     public void testWindows() {
 
     }
 
+    @Test
+    @EnabledOnJre({JRE.JAVA_16})
     public void testJRE() {
 
     }
 
+    @Test
+    @DisabledOnJre({JRE.JAVA_16})
     public void testNoJRE16() {
         
     }
